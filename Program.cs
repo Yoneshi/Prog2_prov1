88d5052b70cb6bcb3bfad3f1996879881b0a2421
Yoneshi <treangpusri@gmail.com>
2019-10-03 15:49:00 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Antikvariatägarens_dotter
 {
-
+    // I denna klass har jag skapat alla variabler för Book
     class Book
     {
         public int price;
@@ -16,13 +16,35 @@ namespace Antikvariatägarens_dotter
         private int actualValue;
         private bool cursed;
 
+        Random generator = new Random(); 
+        // Denna publika konstruktor finns för att ge världen till alla variabler som skapats i klassen.
+        public Book()
+        {
+            price = generator.Next(200);
 
+
+        }
+        public void print()
+        {
+            Console.WriteLine("Priset för denna bok är: " + price);
+
+
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
+            Book bok = new Book();
+            bok.print();
+
+
+            Console.WriteLine(bok);
+
+
+            Console.ReadLine();
+
         }
     }
 }
